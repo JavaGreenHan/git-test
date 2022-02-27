6c2ab180f5145d1d316a3eee54b0fddfd473299e
Thewolf <2436629334@qq.com>
2022-02-27 21:40:23 +0800

    push test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hot fix");
         System.out.println("hot-fix3");
         System.out.println("master");
+        System.out.println("push test");
     }
 }
